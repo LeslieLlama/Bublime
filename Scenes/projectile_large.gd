f2efd9dfdaaad2c9dd3b73620a3fe0f3fd70cc76
LeslieLlama <leslieundinecrooks@gmail.com>
2025-01-26 15:52:49 +1300

    StarKing bugfix
    fixed a bug where a projectile looked wrong

--- Scenes/projectile_large.gd
+++ Scenes/projectile_large.gd
@@ -5,16 +5,20 @@ extends "res://Scripts/enemy_rush.gd"
 func _ready():
 	health = 1
 	speed = 80000
-	if (direction == Vector2.UP || direction == Vector2.DOWN || direction == Vector2.LEFT || direction == Vector2.RIGHT):
-		$Sprite2D.frame = 0
-	else:
-		$Sprite2D.frame = 1
-	$Sprite2D.rotation_degrees = (rad_to_deg(direction.angle())+90)
+	$SpikeStraight.rotation_degrees = (rad_to_deg(direction.angle())+90)
+	$SpikeDiagonal.rotation_degrees = (rad_to_deg(direction.angle())+135)
 	await get_tree().create_timer(2).timeout
 	queue_free()
 	
 func _set_direction(vector):
 	direction = vector
+	
+func _set_sprite(diagonal):
+	print(diagonal)
+	if diagonal == false:
+		$SpikeStraight.show()
+	else: 
+		$SpikeDiagonal.show()
 
 func _physics_process(delta):
 	apply_force(direction * speed, Vector2.ZERO)
